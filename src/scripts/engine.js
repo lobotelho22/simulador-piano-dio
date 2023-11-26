c6c4eaeace3a7ee47bd9c1773cfb62aae69878f0
lobotelho22 <eduardo.ocara@gmail.com>
2023-11-26 09:49:52 -0300

    dev: teclas de piano configuradas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,13 +1,25 @@
 const  pianoKeys = document.querySelectorAll(".piano-keys .key")
 
 let audio = new Audio()
+const mapedKeys = [];
 
 const playTune = (key) => {
     audio.src = `./src/tunes/${key}.wav`
     audio.play()
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+    clickedKey.classList.add("active")
+    setTimeout(() => {
+        clickedKey.classList.remove("active")
+    }, 150)
 }
 
 pianoKeys.forEach((key) => {
-    console.log(key.dataset.key)
     key.addEventListener("click", () => playTune(key.dataset.key))
+    mapedKeys.push(key.dataset.key)
+})
+
+document.addEventListener("keydown", (e) => {
+    if (mapedKeys.includes(e.key)) {
+        playTune(e.key.toLowerCase())
+    }
 })
