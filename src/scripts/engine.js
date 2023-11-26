28392ff8ce4dacb53967ea9983220921c0ace723
lobotelho22 <eduardo.ocara@gmail.com>
2023-11-26 08:37:04 -0300

    dev: adicionado som às teclas do piano

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,13 @@
-const  pianoKeys = document.querySelectorAll(".piano-keys li")
+const  pianoKeys = document.querySelectorAll(".piano-keys .key")
+
+let audio = new Audio()
+
+const playTune = (key) => {
+    audio.src = `./src/tunes/${key}.wav`
+    audio.play()
+}
 
 pianoKeys.forEach((key) => {
     console.log(key.dataset.key)
+    key.addEventListener("click", () => playTune(key.dataset.key))
 })
